6c26c84b8f833f0bbc712ceb163cd07bd93f32a6
Nick Bauman <nick.bauman@cirrusmachina.com>
2013-01-18 22:42:42 -0600

    Generating Avahi services fixed

--- install.py
+++ install.py
@@ -1,11 +1,33 @@
 #!/usr/bin/python
 
+"""
+Copyright (c) 2013 Nick Bauman
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+
 import apt_pkg, apt, re, mmap
 
 from subprocess import call
 from tempfile import mkstemp
 from shutil import move, copy
-from os import remove, path, mkdir, close
+from os import remove, path, mkdir, close, chdir, getcwd
 
 
 INST_PKG_LIST = ["git", "cups", "python-cups", "samba", "avahi-daemon"]
@@ -102,7 +124,9 @@ def create_avahi_srvs():
     print "Fetching airprint-generate." 
     call(["git", "clone", "https://github.com/tjfontaine/airprint-generate"])
   print "Generating airprint services."
-  call(["python", "airprint-generate/airprint-generate.py"])
+  last_dir = getcwd()
+  chdir('/etc/avahi/services/')
+  call(["python", "".join([last_dir, "/airprint-generate/airprint-generate.py"])])
 
 def create_print_share():
   if path.isdir(SMB_SHARE_PATH):
